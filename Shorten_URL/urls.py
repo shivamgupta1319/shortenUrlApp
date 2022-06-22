f28865107961ef55b856d1253c86aae269c35d73
shivamgupta1319 <sg247938@gmail.com>
2022-06-22 15:01:48 +0530

    Creating shorten url web application

---
+++ Shorten_URL/urls.py
@@ -0,0 +1,11 @@
+# config/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Shortener Urls
+    path('', include('Home.urls'))
+]
